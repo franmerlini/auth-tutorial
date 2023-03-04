dabb1214ff5e8fd619cdd006546c2cbe73a9644b
Francisco <merlinifrancisco@gmail.com>
2023-03-04 00:52:25 -0300

    feat: add database and orm configuration

--- db/init.sql
+++ db/init.sql
@@ -1,3 +1,3 @@
 -- Create DB if not exists
-SELECT 'CREATE DATABASE auth-tutorial'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'auth-tutorial')
+SELECT 'CREATE DATABASE authtutorial'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'authtutorial')\gexec
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DataSoruceConfig } from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `./src/environments/.${process.env.NODE_ENV}.env`,
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      ...DataSoruceConfig,
     }),
   ],
   controllers: [AppController],
